4572585dee8a350085675fadcfd5c51c198410d0
Jon Poehlman <jon.poehlman@gmail.com>
2016-02-22 02:42:30 +0000

    new layout

--- app.rb
+++ app.rb
@@ -1,15 +1,42 @@
 require 'sinatra'
 require 'slim'
-#require "sass"
 
 
+configure :development do
+  set :bind, '0.0.0.0'
+  set :port, 3000
+end
 
 class AddressBook < Sinatra::Base
-
- #get('/styles.css') {css :styles }
-
 
   get '/' do
     slim :home
   end
+
+  get '/login' do
+    slim :login
+  end
+
+  post '/login' do
+    if params[:username] == settings.username && params[:password] == settings.password
+      session[:admin] = true
+      redirect to('/about')
+    else
+      slim :login
+    end
+  end
+
+  get '/about'  do
+    @title = "All About This Website"
+     slim :about
+  end
+
+  not_found do
+  	slim :not_found
+  end
+
+
+
+
+
 end
