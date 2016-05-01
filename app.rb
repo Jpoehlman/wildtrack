7a83b7b310bce1b01e9807208c2c47f6281124fc
Jon Poehlman <jon.poehlman@gmail.com>
2016-05-01 16:52:23 +0000

    5-1

--- app.rb
+++ app.rb
@@ -3,16 +3,12 @@ require 'sinatra'
 #require 'sinatra/reloader'
 require 'slim'
 require './soundtrack'
-require 'dm-postgres-adapter'
+
 
 ## Configure
 configure :development do
   DataMapper.setup(:default, "sqlite3://#{ Dir.pwd}/development.db")
 end
-
-
-
-
 
 configure :production do
   DataMapper.setup(:default, ENV['DATABASE_URL'])
